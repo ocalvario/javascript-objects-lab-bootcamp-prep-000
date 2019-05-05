40d813b4a69368a4ee43615aa2b73257d745cd7d
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-05 00:38:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign ({}, object, {[key]: value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  Object[key] = 'value';
+  return object;
+}
