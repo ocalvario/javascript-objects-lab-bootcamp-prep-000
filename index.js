33debd3959de02bb9f409b8e99ba05c30899db05
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-05 00:40:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue (object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  Object[key] = 'value';
+  Object[key] = value;
   return object;
 }
