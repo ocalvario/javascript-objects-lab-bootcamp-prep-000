b3d270c9c0e6321c2ec1cb9d18c7b6ec3e61b27d
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-05 00:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 }
 
 function deleteFromObjectByKey (object,key) {
-  delete object[key];
-  return object;
+  var newObject = Object.assign({}, object)
+  delete newObject[key];
+  return newObject;
 }
 
 function destructivelyDeleteFromObjectByKey (object,key) {
